213719c147645a68084ee193624a5758d217a5e5
Rösch <moritz.roesch@dlr.de>
2024-12-19 13:55:41 +0100

    uncommented import

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
-from scientific_se_projectexample import example
+#from scientific_se_projectexample import example
 
 def test_main():
     assert(True)
 
-example.print_something("hello")
+#example.print_something("hello")
