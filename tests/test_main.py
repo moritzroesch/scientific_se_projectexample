aaa7a4ca8523603c25cdf982545578c00f9aa1d4
Mahakalkar <amruta.mahakalkar@dlr.de>
2024-12-19 14:59:13 +0100

    added substract fun and test

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,4 +2,6 @@ from scientific_se_projectexample import calculator
 
 def test_add():
     assert calculator.add(2,5) == 7
-
+def test_subtract():
+    assert calculator.subtract(5, 3) == 2
+    assert calculator.subtract(0, 3) == -3
